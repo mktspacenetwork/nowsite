cb8c9e1e318e6ab14a5055a45ca7dd3c618a87a3
[dyad] <marketingspacenetwork@gmail.com>
2025-11-18 16:04:31 -0300

    [dyad] Adjusted product layout to 3 columns - wrote 4 file(s)

--- components/pages/AreaAcessoPage.tsx
+++ components/pages/AreaAcessoPage.tsx
@@ -35,7 +35,7 @@ const AreaAcessoPage: React.FC = () => {
 
             <section className="pb-24 bg-background-light dark:bg-background-dark">
                 <div className="container mx-auto px-6">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                         {products.map((product, index) => (
                             <div 
                                 key={index} 
